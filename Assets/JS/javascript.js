b1c29f98d505c8ab815cc652b6836d8eca8cfb72
Logan Davis <Ldavis96@live.com>
2021-11-21 20:20:38 -0500

    modified cloned write password function to imrpove functionality

--- Assets/JS/javascript.js
+++ Assets/JS/javascript.js
@@ -139,16 +139,12 @@ function generatePassword() {
 }
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
+    var password1 = "";
+    password1 = generatePassword();
     var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
+    passwordText.value = password1;
 }
 
 // Add event listener to generate button
